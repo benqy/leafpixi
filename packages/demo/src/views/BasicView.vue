e7e12303a0b5194df2895e972d6cfab0ddad64a9
benqy <hmjlr123@gmail.com>
2024-07-08 22:30:31 +0800

    init

--- packages/demo/src/views/BasicView.vue
+++ packages/demo/src/views/BasicView.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>
-    2
+  <div class="content">
+    <div id="map"></div>
   </div>
 </template>
 <script setup lang="ts">
+import { Map, TileLayer } from 'leaflet'
+import { onMounted } from 'vue'
+
+const initMap = () => {
+  const map = new Map('map', {
+    attributionControl: false
+  }).setView([51.505, -0.09], 13)
+  new TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+  }).addTo(map)
+  return map
+}
+
+onMounted(() => {
+  initMap()
+})
 </script>
-<style scoped lang="scss">
+<style scoped>
+#map,
+.content {
+  width: 100vw;
+  height: 100vh;
+}
 </style>
---
+++ packages/map/plugins/webglMarker.ts
@@ -0,0 +1,3 @@
+import * as PIXI from 'pixi.js'
+import { Map } from 'leaflet'
+export class WebglMarker {}
