20d25de42c905a11dca16db567f641571efdc730
benqy <hmjlr123@gmail.com>
2024-07-09 00:49:38 +0800

    treeshake for shared packages

--- packages/server/src/app.controller.ts
+++ packages/server/src/app.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ClassDemo } from '@leafpixi/common';
-
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
+import { ClassDemo } from '@leafpixi/common'
+import { utilFn } from '@leafpixi/common/utils'
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    const demo: ClassDemo = new ClassDemo();
-    return JSON.stringify(demo);
+    const demo: ClassDemo = new ClassDemo()
+    const utilR = utilFn('wgwgw')
+    return JSON.stringify(demo) + utilR
   }
 }
