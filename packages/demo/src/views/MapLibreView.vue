f105ca791a500545b6b002d4e6d64650192e7d6a
benqy <hmjlr123@gmail.com>
2024-07-09 20:56:55 +0800

    marker demo

--- packages/demo/src/views/MapLibreView.vue
+++ packages/demo/src/views/MapLibreView.vue
@@ -7,21 +7,22 @@
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import { onMounted } from 'vue'
+let map: maplibregl.Map
 const initMap = () => {
-  const map = new maplibregl.Map({
+  map = new maplibregl.Map({
     container: 'map', // container id
     style: {
       version: 8,
       sources: {
         'raster-tiles': {
           type: 'raster',
-          tiles: ['https://xxxxxxx/{z}/{y}_{x}.png'],
+          tiles: ['https://xxxxxx/{z}/{y}_{x}.png'],
           bounds: [
             -1.4,
             0, // Southwest coordinates
             0,
             1.4 // Northeast coordinates
-          ],
+          ]
         }
       },
       layers: [
@@ -44,28 +45,62 @@ const initMap = () => {
     center: [-0.7, 0.7], // starting position
     zoom: 9 // starting zoom
   })
-  map.on('load', () => {
-    map.fitBounds([
-      [-1.4, 0], // Southwest coordinates
-      [0, 1.4] // Northeast coordinates
-    ])
-  })
-  map.on('click', (e) => {
-    console.log(e.lngLat)
-  })
-  map.on('zoom', () => {
-    console.log(map.getZoom())
-    // if (map.getZoom() > map.getMaxZoom()) {
-    //   map.zoomTo(map.getMaxZoom())
-    // }
-  })
-  map.on('contextmenu', (e) => {
-    console.log(e.lngLat)
-  })
+  // map.on('load', () => {
+  //   map.fitBounds([
+  //     [-1.4, 0], // Southwest coordinates
+  //     [0, 1.4] // Northeast coordinates
+  //   ])
+  // })
+  // map.on('click', (e) => {
+  //   console.log(e.lngLat)
+  // })
+  // map.on('zoom', () => {
+  //   console.log(map.getZoom())
+  //   // if (map.getZoom() > map.getMaxZoom()) {
+  //   //   map.zoomTo(map.getMaxZoom())
+  //   // }
+  // })
+  // map.on('contextmenu', (e) => {
+  //   console.log(e.lngLat)
+  // })
+}
+
+const addMarkers = () => {
+  const lng = Math.random() * -1.4
+  const lat = Math.random() * 1.4
+  new maplibregl.Marker()
+    .setLngLat([lng, lat])
+    .addTo(map)
+  // const marker = {
+  //   type: 'Feature',
+  //   properties: {
+  //     message: 'Foo',
+  //     iconSize: [60, 60]
+  //   },
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [-0.7, 0.7]
+  //   }
+  // }
+  // const el = document.createElement('div')
+  // el.className = 'marker'
+  // el.style.backgroundImage = `url(https://picsum.photos/${marker.properties.iconSize.join('/')}/)`
+  // el.style.width = `${marker.properties.iconSize[0]}px`
+  // el.style.height = `${marker.properties.iconSize[1]}px`
+
+  // el.addEventListener('click', () => {
+  //   window.alert(marker.properties.message)
+  // })
+
+  // // add marker to map
+  // new maplibregl.Marker({ element: el }).setLngLat([-0.7, 0.7]).addTo(map)
 }
 
 onMounted(() => {
   initMap()
+  for(let i =0;i<2000;i++){
+    addMarkers()
+  }
 })
 </script>
 <style scoped>
